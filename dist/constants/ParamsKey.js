938723bcdaf7d63e5888c5408e0e6134c165733d
liampham <liam.pham@grapecity.com>
2019-03-13 14:22:07 +0700

    Generate JS Code

---
+++ dist/constants/ParamsKey.js
@@ -0,0 +1,21 @@
+var ParamsKey = /** @class */ (function () {
+    function ParamsKey() {
+    }
+    ParamsKey._NAME_ = "name";
+    ParamsKey._POSITION_ = "position";
+    ParamsKey._FORE_COLOR_ = "fore_color";
+    ParamsKey._STATE_ = "state";
+    ParamsKey._ON_IMAGE_ = "on_image";
+    ParamsKey._OFF_IMAGE_ = "off_image";
+    ParamsKey._X_ = "x";
+    ParamsKey._Y_ = "y";
+    ParamsKey._POWER_STATE_ = "power_state";
+    ParamsKey._GRID_COLUMN_ = "grid_column";
+    ParamsKey._GRID_ROW_ = "grid_row";
+    ParamsKey._BACKGROUND_COLOR_ = "background_color";
+    ParamsKey._DISPLAY_COMPONENT_NAME_ = "display_component_name";
+    ParamsKey._ELECTRIC_COMPONENTS_ = "electric_components";
+    ParamsKey._PLUGGED_IN_STATE_ = "plugged_in_state";
+    ParamsKey._CUSTOME_RENDER_ = "custom_render";
+    return ParamsKey;
+}());
---
+++ dist/constants/R.js
@@ -0,0 +1,26 @@
+var R = /** @class */ (function () {
+    function R() {
+    }
+    R.BTN_APPLY_BOARD_CONFIGURATION = "btn_apply_board_configuration";
+    R.BTN_CLOSE_COMPONENT_DETAIL = "btn_close_component_detail";
+    R.BTN_ADD_COMPONENT = "btn_add_component";
+    R.BTN_SUBMIT_COMPONENT = "btn_submit_component";
+    R.INPUT_COMPONENT_NAME = "input_component_name";
+    R.LIST_COMPONENTS_CONTAINER = "list_components_container";
+    R.COMPONENTS_CONTAINER = "components_container";
+    R.COMPONENT_DETAIL_CONTAINER = "component_detail_container";
+    R.INPUT_BOARD_DISPLAY_COMPONENT_NAME = "input_board_display_component_name";
+    R.INPUT_BOARD_POWER_SWITCH = "input_board_power_switch";
+    R.INPUT_BOARD_GRID_ROW = "input_board_grid_row";
+    R.INPUT_BOARD_GRID_COLUMN = "input_board_grid_column";
+    R.INPUT_BOARD_BACKGROUND_COLOR = "input_board_background_color";
+    R.COMPONENT_NAME = "component_name";
+    R.INPUT_CUSTOM_RENDER = "input_custom_render";
+    R.INPUT_COMPONENT_POSITION_X = "input_component_position_x";
+    R.INPUT_COMPONENT_POSITION_Y = "input_component_position_y";
+    R.INPUT_COMPONENT_ON_IMAGE = "input_component_on_image";
+    R.INPUT_COMPONENT_OFF_IMAGE = "input_component_off_image";
+    R.INPUT_COMPONENT_FORECOLOR = "input_component_forecolor";
+    R.INPUT_COMPONENT_POWER_STATE = "input_component_power_state";
+    return R;
+}());
